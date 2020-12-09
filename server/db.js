34877174ff1a8f80ce3f7d4fa3b3e829c6b0a266
DharanShah2468 <73468658+DharanShah2468@users.noreply.github.com>
2020-12-09 13:18:04 +0530

    Update db.js

--- server/db.js
+++ server/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose')
 
-mongoose.connect('mongodb+srv://dharan246:zxcvbnm.2468@cluster0.gtljh.mongodb.net/UserInfo?retryWrites=true&w=majority',{useNewUrlParser:true,useUnifiedTopology:true},
+mongoose.connect('enter your mongo uri',{useNewUrlParser:true,useUnifiedTopology:true},
     err => {
         if (!err)
             console.log('Mongodb connection succeeded.')
